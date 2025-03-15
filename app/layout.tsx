c8ded2879b11299552d7105ae88dc6f2600ff9d1
iaaditya13 <aadityadubey1305@gmail.com>
2025-03-16 00:00:58 +0530

    test commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'EzFunds',
+  title: 'EzEfunds',
   description: 'Investify',
   generator: 'Next.js',
 }
